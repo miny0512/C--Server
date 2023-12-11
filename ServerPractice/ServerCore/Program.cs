5a6594ca420bcf1425225ed4a0b23f3f87a3107e
miny0512 <nameless1029@naver.com>
2023-12-11 18:30:40 +0900

    쓰레드 생성

--- ServerPractice/ServerCore/Program.cs
+++ ServerPractice/ServerCore/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace ServerCore
 {
-    internal class Program
+    class Program
     {
+        static void MainThread(object state)
+        {
+            for(int i = 0; i < 5; ++i)
+            {
+                Console.WriteLine("Hello Thread!");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 백그라운드
+            //ThreadPool.QueueUserWorkItem(MainThread);
+
+
+            for(int i = 0; i< 1000; ++i)
+            {
+                Thread t = new Thread(MainThread);
+                t.IsBackground = true;
+                t.Start();
+            }
+            //t.Join();
+            //Console.WriteLine("Hello World!");
         }
     }
 }
